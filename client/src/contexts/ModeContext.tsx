71e469dd8ee843888126abfa92802c4be0c5e718
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2025-01-05 00:21:12 +0330

    feat: ModeContext

--- client/src/contexts/ModeContext.tsx
+++ client/src/contexts/ModeContext.tsx
@@ -1,26 +1,25 @@
 import React, { createContext, PropsWithChildren, useContext, useState, useMemo } from "react";
-
-type Mode = "work" | "shortRest" | "longRest";
+import { Mode } from "../utils/types";
 
 type ModeContextTypes = {
   mode: Mode;
   color: string;
-  time: string;
+  initialTime: string;
   setMode: React.Dispatch<React.SetStateAction<Mode>>;
 };
 
-const modeSettings: Record<Mode, { color: string; time: string }> = {
-  work: { color: "#e76f51", time: "00:25:00" },
-  shortRest: { color: "#f4a261", time: "00:05:00" },
-  longRest: { color: "#2a9d8f", time: "00:15:00" },
+const modeSettings: Record<Mode, { color: string; initialTime: string }> = {
+  work: { color: "#3A3F47", initialTime: "00:25:00" },
+  shortRest: { color: "#B9A7D1", initialTime: "00:05:00" },
+  longRest: { color: "#2a9d8f", initialTime: "00:15:00" },
 };
 
 const ModeContext = createContext<ModeContextTypes | undefined>(undefined);
 
 export default function ModeProvider({ children }: PropsWithChildren) {
   const [mode, setMode] = useState<Mode>("work");
-  const { color, time } = useMemo(() => modeSettings[mode], [mode]);
-  return <ModeContext.Provider value={{ mode, color, time, setMode }}>{children}</ModeContext.Provider>;
+  const { color, initialTime } = useMemo(() => modeSettings[mode], [mode]);
+  return <ModeContext.Provider value={{ mode, color, initialTime, setMode }}>{children}</ModeContext.Provider>;
 }
 
 export function useMode() {
